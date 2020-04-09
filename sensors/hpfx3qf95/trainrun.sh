27c321d3134584e6cbc45c567135ad2588bfab60
Styxxxxx <yuting_du@126.com>
2020-04-09 17:28:08 +0800

    222

---
+++ sensors/hpfx3qf95/trainrun.sh
@@ -0,0 +1,2 @@
+#../caffe_hpfx3/build/tools/caffe train --solver=solver.prototxt --gpu 0 1> train-log.txt 2>&1
+/home/qiaotong/caffe/build/tools/caffe train --solver=solver_new.prototxt 1> train-log-2.txt 2>&1
